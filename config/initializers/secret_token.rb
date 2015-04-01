e89782ded4e666d59b559654614040b4ef21cfc3
Paresh Ghodge <psghodge@gmail.com>
2015-03-31 21:36:10 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Athletrak::Application.config.secret_key_base = '9cfcbd53b0db192d40d595a82f51ff5666f5dfb877bed6c45b74b4a2e24807a2e913b9bdb1edb1ce95825f913f2cae95e5c0a993b6a29f17f8b59cf8ae12ab7d'
